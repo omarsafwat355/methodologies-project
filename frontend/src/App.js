b3a88789070c6ba6a47a7980bb0f3d41f044bdab
omar <omarsafwat355@gmail.com>
2026-05-14 00:03:25 +0300

    Initial commit: Add frontend, backend containers and docker-compose

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Methodology Project 2</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
